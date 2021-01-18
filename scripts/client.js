293cf66858b6d00230b5bcdcfbe8615a0d8517a9
Ben Keener <kiba.nightfall@gmail.com>
2021-01-18 10:53:11 -0700

    Minor udpates to client.js made.

--- scripts/client.js
+++ scripts/client.js
@@ -1,10 +1,12 @@
-var clearbtn = document.getElementById('clearbtn');
-var dbbtn = document.getElementById('dumpbtn');
+const clearbtn = document.getElementById("clearbtn");
+const dbbtn = document.getElementById("dumpbtn");
+const databaseServer = "localhost:5000"
+const database = "dev/db"
 var table = document.querySelector("table");
 
 function generateTableHead(table, data) {
-    let thead = table.createTHead();
-    let row = thead.insertRow();
+    let tableHead = table.createTHead();
+    let row = tableHead.insertRow();
     for (let key of data) {
         let th = document.createElement("th");
         let text = document.createTextNode(key);
@@ -25,7 +27,7 @@ function generateTable(table, data) {
 }
 
 clearbtn.onclick = function () {
-    console.log(String(table.rows.length) + ' rows to delete')
+    console.log(String(table.rows.length) + " rows to delete")
     for (i = table.rows.length - 1; i > -1; i--) {
         table.deleteRow(i);
     }
@@ -33,12 +35,11 @@ clearbtn.onclick = function () {
 
 dumpbtn.onclick = function () {
     const request = new XMLHttpRequest();
-    const url = 'http://localhost:5000/dev/db';
-    request.open('GET', url);
+    const url = "http://" + databaseServer + "/" + database;
+    request.open("GET", url);
     request.send();
     request.onload = (e) => {
-        temp = JSON.parse(request.response)['result'];
-        //let table = document.querySelector("table");
+        temp = JSON.parse(request.response)["result"];
         let data = Object.keys(temp[0]);
         generateTable(table, temp);
         generateTableHead(table, data);
